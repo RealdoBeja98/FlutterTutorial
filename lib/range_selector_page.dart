f0b845cccedcc2d602459dd86ebe53a7aeb0d5c5
RealdoBeja98 <realdo.beja@mail.polimi.it>
2023-02-23 16:55:10 +0100

    First commit

---
+++ lib/range_selector_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class RangeSelectorPage extends StatefulWidget {
+  const RangeSelectorPage({Key? key}) : super(key: key);
+
+  @override
+  State<RangeSelectorPage> createState() => _RangeSelectorPageState();
+}
+
+class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Select Range'),
+        ),
+      ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RangeSelectorTextFormField(
+                labelText: 'Minimum',
+              ),
+              SizedBox(height: 12),
+              RangeSelectorTextFormField(
+                labelText: 'Maximum',
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_forward),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class RangeSelectorTextFormField extends StatelessWidget {
+  const RangeSelectorTextFormField({
+    super.key,
+    required this.labelText,
+  });
+
+  final String labelText;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: labelText,
+      ),
+      keyboardType: TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
+      ),
+    );
+  }
+}
